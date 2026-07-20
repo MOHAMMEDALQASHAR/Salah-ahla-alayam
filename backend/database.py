f5d060aa24d90a539bab6b660e49ca8638ebfe3b
MOHAMMED ALQASHAR <alqashar2002@gmail.com>
2026-07-21 00:15:53 +0300

    Update default MongoDB URI in database.py

--- backend/database.py
+++ backend/database.py
@@ -2,16 +2,15 @@ import os
 from pymongo import MongoClient
 
 DEFAULT_URI = (
-    "mongodb://alqashar0_db_user:Mtm775070_981@"
-    "ac-rfe4z7p-shard-00-00.gcalylm.mongodb.net:27017,"
-    "ac-rfe4z7p-shard-00-01.gcalylm.mongodb.net:27017,"
-    "ac-rfe4z7p-shard-00-02.gcalylm.mongodb.net:27017/"
-    "ahla_alayam?ssl=true&authSource=admin&retryWrites=true&w=majority"
+    "mongodb+srv://alqashar0_db_user:Mtm775070_981@"
+    "cluster0.gcalylm.mongodb.net/ahla_alayam?retryWrites=true&w=majority&appName=Cluster0"
 )
 
-MONGODB_URI = os.getenv("MONGODB_URI", DEFAULT_URI)
+MONGODB_URI = os.getenv("MONGODB_URI", DEFAULT_URI).strip()
+if not MONGODB_URI:
+    MONGODB_URI = DEFAULT_URI
 
-client = MongoClient(MONGODB_URI)
+client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=10000)
 
 # Database instance
 db_name = os.getenv("MONGODB_DB_NAME", "ahla_alayam")
